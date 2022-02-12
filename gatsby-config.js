eca7015241b16bc7f2295125420d3001e3f8c6fd
identity16 <dnjswns0930@gmail.com>
2022-02-12 23:14:48 +0900

    Make RSS feed data

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,7 @@ require("dotenv").config({
 
 module.exports = {
     siteMetadata: {
-        siteUrl: "https://identity16.github.io",
+        siteUrl: "https://identity16.github.io/blog/",
         title: "identity16",
     },
     plugins: [
@@ -23,6 +23,66 @@ module.exports = {
             extensions: [`.md`, `.mdx`],
             options: {
                 gatsbyRemarkPlugins: ["gatsby-remark-line-breaks"],
+            },
+        },
+        {
+            resolve: "gatsby-plugin-feed",
+            options: {
+                query: `
+                  {
+                    site {
+                      siteMetadata {
+                        title
+                        description
+                        siteUrl
+                        site_url: siteUrl
+                      }
+                    }
+                  }
+                `,
+                feeds: [
+                    {
+                        serialize: ({ query: { site, allMdx } }) => {
+                            return allMdx.edges.map((edge) => {
+                                return Object.assign(
+                                    {},
+                                    edge.node.frontmatter,
+                                    {
+                                        description:
+                                            edge.node.frontmatter.description,
+                                        date: edge.node.frontmatter.date,
+                                        url:
+                                            site.siteMetadata.siteUrl +
+                                            "/" +
+                                            edge.node.id,
+                                        guid:
+                                            site.siteMetadata.siteUrl +
+                                            "/" +
+                                            edge.node.id,
+                                    }
+                                );
+                            });
+                        },
+                        query: `
+                        {
+                            allMdx(sort: { order: DESC, fields: [frontmatter___date] },) {
+                                edges {
+                                    node {
+                                        id
+                                        frontmatter {
+                                            title
+                                            description
+                                            date
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                        `,
+                        output: "/rss.xml",
+                        title: "identity16's BLOG",
+                    },
+                ],
             },
         },
     ],

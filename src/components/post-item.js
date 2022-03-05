0abe56c01e4c25b75b192a5190f1ca4c4ee5b2bb
identity16 <dnjswns0930@gmail.com>
2022-03-05 15:25:25 +0900

    PostItem 컴포넌트 분리

---
+++ src/components/post-item.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { GatsbyImage } from "gatsby-plugin-image";
+import { Link } from "gatsby";
+import {
+    postItem,
+    featuredImage,
+    infoContainer,
+    category,
+    title,
+    description,
+    date,
+} from "./post-item.module.css";
+
+function PostItem({ node }) {
+    return (
+        <article className={postItem}>
+            <Link to={`/blog/${node.slug}`}>
+                {node.frontmatter.featuredImage && (
+                    <GatsbyImage
+                        className={featuredImage}
+                        image={
+                            node.frontmatter.featuredImage.childImageSharp
+                                .gatsbyImageData
+                        }
+                        alt="Featured Image"
+                    />
+                )}
+                <div className={infoContainer}>
+                    {node.frontmatter.category && (
+                        <span to="/" className={category}>
+                            {node.frontmatter.category}
+                        </span>
+                    )}
+                    <h2 className={title}>{node.frontmatter.title}</h2>
+                    {node.frontmatter.description && (
+                        <p className={description}>
+                            {node.frontmatter.description}
+                        </p>
+                    )}
+                    <p className={date}>{node.frontmatter.date}</p>
+                </div>
+            </Link>
+        </article>
+    );
+}
+
+export default PostItem;
--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,50 +1,15 @@
-import { graphql, Link } from "gatsby";
+import { graphql } from "gatsby";
 import React from "react";
 import Layout from "../../components/layout";
-import {
-    postList,
-    postItem,
-    featuredImage,
-    infoContainer,
-    category,
-    title,
-    description,
-    date,
-} from "./list.module.css";
-import { GatsbyImage } from "gatsby-plugin-image";
+import { postList } from "./list.module.css";
+
+import PostItem from "../../components/post-item";
 
 function BlogPage({ data: { allMdx } }) {
     return (
         <Layout pageTitle="Blog Posts" className={postList}>
             {allMdx.nodes.map((node) => (
-                <article key={node.slug} className={postItem}>
-                    <Link to={`/blog/${node.slug}`}>
-                        {node.frontmatter.featuredImage && (
-                            <GatsbyImage
-                                className={featuredImage}
-                                image={
-                                    node.frontmatter.featuredImage
-                                        .childImageSharp.gatsbyImageData
-                                }
-                                alt="Featured Image"
-                            />
-                        )}
-                        <div className={infoContainer}>
-                            {node.frontmatter.category && (
-                                <span to="/" className={category}>
-                                    {node.frontmatter.category}
-                                </span>
-                            )}
-                            <h2 className={title}>{node.frontmatter.title}</h2>
-                            {node.frontmatter.description && (
-                                <p className={description}>
-                                    {node.frontmatter.description}
-                                </p>
-                            )}
-                            <p className={date}>{node.frontmatter.date}</p>
-                        </div>
-                    </Link>
-                </article>
+                <PostItem key={node.slug} node={node} />
             ))}
         </Layout>
     );
